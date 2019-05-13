ee2ca720fd62c46abd6359cba610b1bdaab0a311
Mme Mhiri Lina <mhiri@insa-10220>
2019-05-13 10:23:52 +0200

    fff

--- 2.RISC_Processor/risc_processor_v1/ALU.vhd
+++ 2.RISC_Processor/risc_processor_v1/ALU.vhd
@@ -19,15 +19,16 @@
 ----------------------------------------------------------------------------------
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.STD_LOGIC_ARITH.ALL;
 
 -- Uncomment the following library declaration if using
 -- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
+use IEEE.NUMERIC_STD.ALL;
 
 -- Uncomment the following library declaration if instantiating
 -- any Xilinx primitives in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
+library UNISIM;
+use UNISIM.VComponents.all;
 
 -- Use our library 
 library work;
@@ -40,12 +41,12 @@ entity ALU is
            B        : in  STD_LOGIC_VECTOR  (TAILLE_REGISTRES-1 downto 0);
            S        : out  STD_LOGIC_VECTOR (TAILLE_REGISTRES-1 downto 0);
            FLAGS    : out  STD_LOGIC_VECTOR (4 downto 0));
-           -- FLAGS = (N, O, Z, C)
+        -- FLAGS = (N, O, Z, C)
 end ALU;
 
 architecture Behavioral of ALU is
 
-    signal  S_add   : STD_LOGIC_VECTOR(N     downto 0);
+   signal  S_add   : STD_LOGIC_VECTOR(N     downto 0);
 	signal  S_sub   : STD_LOGIC_VECTOR(N-1   downto 0);
 	signal  S_mul   : STD_LOGIC_VECTOR(N*2-1 downto 0);
 	signal  S_eqz   : STD_LOGIC;
@@ -55,12 +56,12 @@ architecture Behavioral of ALU is
 	constant Zeros  : STD_LOGIC_VECTOR(N*2-1 downto 0) := (others => '0');
 
 begin
-	S 	<=  S_add(N-1 downto 0) when Ctrl_ALU="001" else
-			S_sub 				when Ctrl_ALU="010" else
-			S_mul(N-1 downto 0) when Ctrl_ALU="011" else
+	S 	<= S_add(N-1 downto 0) 				when Ctrl_ALU="001" else
+			S_sub 								when Ctrl_ALU="010" else
+			S_mul(N-1 downto 0) 				when Ctrl_ALU="011" else
 			Zeros(N-2 downto 0) & S_eqz 	when Ctrl_ALU="100" else
-			Zeros(N-2 downto 0) & S_noz		when Ctrl_ALU="101" else
-			Zeros(N-2 downto 0) & S_inf		when Ctrl_ALU="110" else
+			Zeros(N-2 downto 0) & S_noz	when Ctrl_ALU="101" else
+			Zeros(N-2 downto 0) & S_inf	when Ctrl_ALU="110" else
 			Zeros(N-1 downto 0) ;
 
 	S_add  <=  ('0' & A) + ('0' & B);
@@ -77,21 +78,21 @@ begin
     -- N : Negative
     FLAGS(0) <= '1'	when Ctrl_ALU="001" and S_add(S_add'high-1) = '1'    else -- S_add
                 '1'	when Ctrl_ALU="010" and S_sub(S_sub'high) = '1'      else -- S_sub
-                '1' when Ctrl_ALU="011" and S_mul(S_mul'high) = '1'      else -- S_mul
+                '1' 	when Ctrl_ALU="011" and S_mul(S_mul'high) = '1'      else -- S_mul
                 '0';
     -- O : Overflow
 	FLAGS(1) <= '1'	when Ctrl_ALU="001" and S_add = x"0"&Zeros(N-1 downto 0)	else -- S_add
-			    '1'	when Ctrl_ALU="010" and S_sub = Zeros(N-1 downto 0) 		else -- S_sub
-                '1'	when Ctrl_ALU="011" and S_mul = Zeros 	else -- S_mul
-			    '0';
+					'1'	when Ctrl_ALU="010" and S_sub = Zeros(N-1 downto 0) 		else -- S_sub
+               '1'	when Ctrl_ALU="011" and S_mul = Zeros 	else -- S_mul
+					'0';
     -- Z : Zero
-    FLAGS(2) <= '1'	when Ctrl_ALU="001" and (A(A'high) = B(B'high) and S_add(S_add'high-1) /= A(A'high))	 				else -- S_add
-			    '1' when Ctrl_ALU="010" and (A(A'high) = '0'	   and B(B'high) = '1' and S_sub(S_sub'high) = '1') 	else -- S_sub
-                '1' when Ctrl_ALU="010" and (A(A'high) = '1'   	   and B(B'high) = '0' and S_sub(S_sub'high) = '0') 	else -- S_sub
-			    '1'	when Ctrl_ALU="011" and S_mul(N*2-1 downto N) /= Zeros(N-1 downto 0) else -- S_mul
-			    '0';
+    FLAGS(2) <= '1' when Ctrl_ALU="001" and (A(A'high) = B(B'high) and S_add(S_add'high-1) /= A(A'high))	 		else -- S_add
+					 '1' when Ctrl_ALU="010" and (A(A'high) = '0' and B(B'high) = '1' and S_sub(S_sub'high) = '1') 	else -- S_sub
+                '1' when Ctrl_ALU="010" and (A(A'high) = '1' and B(B'high) = '0' and S_sub(S_sub'high) = '0') 	else -- S_sub
+					 '1' when Ctrl_ALU="011" and S_mul(N*2-1 downto N) /= Zeros(N-1 downto 0) else -- S_mul
+					 '0';
     -- C : Carry
-	FLAGS(3) <= '1' when Ctrl_ALU="001" and S_add(S_add'high) = '1'      else -- S_add
+	FLAGS(3) <= '1' when Ctrl_ALU="001" and S_add(S_add'high) = '1' else -- S_add
 			    '0';
 
 
